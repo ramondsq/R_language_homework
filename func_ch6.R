d232b640f7b0383d6e41812cbe3e5abe7958bb79
qurui <quruis7@gmail.com>
2021-04-23 12:48:54 +0800

    完成第六章作业

--- func_ch6.R
+++ func_ch6.R
@@ -6,3 +6,13 @@ u.conf.int<-function(x, sigma, conf.level = 0.95){
     Se = sigma / sqrt(n)
     xbar + c(-ua * Se, ua * Se)
 }
+
+square <- function(x, conf.level) {
+    n = length(x)
+    eq = mean(x)
+    stan = sd(x)
+    down = stan / sqrt(n)
+    a = 1 - conf.level
+    t = qt(1 - a / 2, n - 1)
+    eq + c(-t * down, t * down)
+}
